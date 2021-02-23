292295742f8eb6944bfb2054ed244dfe4cb939bd
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-02-23 13:27:19 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+const NoFortnite = require('discord.js')
+const client = new NoFortnite.Client()
+
+client.on('ready' ready => {
+
+  client.guilds.cache.forEach(g => {
+    console.log(`${g.name} is now protected from Fortnite Kids.`)
+  })
+  console.log(`${client.user.username} is now Ready!`)
+});
+
+client.on('message' message => {
+  if(message.content.includes("fortnite")) {
+    message.delete()
+  } else if(message.content.includes("Fortnite")) {
+    message.delete()
+  } else if(message.content.includes("fortnight")) {
+    message.delete()
+  } else if(message.content.includes("Fortnight")) {
+    message.delete()
+  } else console.log("A message was sent, but it wasnt containing fortnite.")
+  
+  console.log(`Deleted a Fortnite Message in ${message.guild.name}`)
+  
+});
+
+client.login('bot-token')
